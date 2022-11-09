d8deae8e6982ea2ecd42e08d95967fe1dae9de01
Alisson Felipe Martim Da Silva <Alissonfelipe1234@outlook.com>
2022-11-09 03:35:19 -0300

    DeadPage result

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import sys
-from classes import DeadPage
 from concurrent.futures import as_completed
 from controller import get_icon, get_phones_number
 from requests_futures.sessions import FuturesSession
@@ -8,12 +7,15 @@ urls = sys.stdin
 session = FuturesSession()
 results = []
 
-futures=[session.get(i) for i in urls]
+futures=[session.get(i.strip()) for i in urls]
 
-for future in as_completed(futures, timeout=5):
-    response = future.result()
-    if response.status_code != 200:
-        results.append(DeadPage)
+for future in as_completed(futures):
+    try:
+        response = future.result(timeout=5)
+        if response.status_code != 200:
+            raise Exception
+    except:
+        results.append("DeadPage")
         continue
     
     text = response.text
--- classes.py
+++
@@ -1,3 +0,0 @@
-class DeadPage(Exception):
-    def __str__(self):
-        return "DeadPage"
